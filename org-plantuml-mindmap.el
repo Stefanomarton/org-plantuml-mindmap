10a3fbd1bf03783d2d5f8013a4fcfd72a1de8e79
Stefanomarton <sstefanomarton@gmail.com>
2024-07-14 23:09:13 +0200

    initial commit

---
+++ org-plantuml-mindmap.el
@@ -0,0 +1,75 @@
+;;; org-plantuml-mindmap.el --- create mindmap quickly. -*- lexical-binding: t -*-
+
+(defgroup org-plantuml-mindmap nil
+  "Quick creation of mindmaps"
+  :group 'convenience)
+
+(defcustom org-plantuml-mindmap-result-type "string"
+  "Result type of org-plantuml-mindmap"
+  :type 'string
+  :group 'org-plantuml-mindmap)
+
+(setq org-plantuml-mindmap-result-type "svg")
+
+(defcustom org-plantuml-mindmap-folder ""
+  "If non-nil, create a folder for the PlantUML mind map files."
+  :type 'string
+  :group 'org-plantuml-mindmap)
+
+(defun org-plantuml-mindmap--formatted-timestamp ()
+  "Return the current date and time as a formatted string: YEAR-MONTH-DAY_HOUR-MINUTE_mindmap."
+  (let ((timestamp (format-time-string "%Y-%m-%d_%H-%M")))
+    (concat timestamp "_mindmap")))
+
+(defun org-plantuml-mindmap-result ()
+  "Check the value of `org-plantuml-mindmap-folder` and set `org-plantuml-mindmap-create' accordingly"
+  (let (org-plantuml-mindmap-folder-name)
+    (cond ((string= org-plantuml-mindmap-result-type "svg")
+           (concat " :file " (org-plantuml-mindmap--folder) (org-plantuml-mindmap--formatted-timestamp) ".svg"))
+          (t
+           (message "not-test")))))
+
+(defun org-plantuml-mindmap-result ()
+  "Check the value of `org-plantuml-mindmap-result-type` and set `org-plantuml-mindmap-create' accordingly"
+  (let (org-plantuml-mindmap-result)
+    (cond ((string= org-plantuml-mindmap-result-type "svg")
+           (concat " :file " org-plantuml-mindmap-folder (org-plantuml-mindmap--formatted-timestamp) ".svg"))
+          (t
+           (message "not-test")))))
+
+
+(defun org-plantuml-mindmap-create ()
+  "Create a PlantUML mind map code block from the current Org buffer."
+  (interactive)
+  (let ((mind-map-code ""))
+    (save-excursion
+      (goto-char (point-min))
+      (while (re-search-forward "^\\(*+ [^\\n]*\\)$" nil t)
+        (setq mind-map-code (concat mind-map-code
+                                    (match-string 1) "\n"))
+        (forward-line 1)))
+    (setq mind-map-code (replace-regexp-in-string "^[*]" ",*" mind-map-code))
+    (setq mind-map-code (replace-regexp-in-string "^[[:blank:]\n]*" "" mind-map-code))
+
+    (let ((folder org-plantuml-mindmap-folder))
+      (unless (file-directory-p folder)
+        (make-directory folder t))
+      folder)
+
+    (insert (concat "#+BEGIN_SRC plantuml" (org-plantuml-mindmap-result) "\n"))
+    (insert (format "@startmindmap\n%s@endmindmap" mind-map-code))
+    (insert "\n#+END_SRC")))
+
+(defun my/org-plantuml-mindmap-create ()
+  "Insert a PlantUML mind map code block in the current Org buffer."
+  (interactive)
+  (setq-local org-plantuml-mindmap-folder (concat
+                                           org-directory
+    			                           org-attachments-folder
+                                           "mindmaps/"
+    			                           (file-name-sans-extension (buffer-name))
+    			                           "/"))
+  (org-plantuml-mindmap-create)
+  (save-buffer))
+
+(provide 'org-plantuml-mindmap)
